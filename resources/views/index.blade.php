1c5cacf5a2566441c41e7948095766af276f307e
t.t.hawkwing@gmail.com <t.t.hawkwing@gmail.com>
2023-02-18 14:16:33 +0900

    first-commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<style>
+  
+</style>
+<body>
+  <form action="/thanks" method="post">
+    <table>
+      <tr>
+        <th>氏名</th><br>
+        <td><input type="text" name="name" /></td>
+      </tr>
+      <tr>
+        <th>メールアドレス</th><br>
+        <td><input tyep="email" name="email" /></td>
+      </tr>
+      <tr>
+        <button type="submit" class="send_button">送信する</button>
+      </tr>
+</body>
+</html>
---
+++ resources/views/thanks.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<style>
+
+</style>
+<body>
+  <p>お問い合わせありがとうございます。</p><br>
+  <a href="/" class="to_the_top">トップへ戻る</a>
+</body>
+</html>
